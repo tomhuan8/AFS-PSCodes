5f34eeb604165d65a2075c29f230515d50154acc
tomhuan8 <137205073+tomhuan8@users.noreply.github.com>
2025-06-13 00:55:32 -0400

    update

--- oobe_before.ps1
+++ oobe_before.ps1
@@ -1,6 +1,4 @@
 #https://raw.githubusercontent.com/tomhuan8/AFS-PSCodes/refs/heads/main/oobe_before.ps1
 
-#Start Windows Update
-Write-Host "Does it work?"
-
-write-host "Maybe"
+#Starting the script
+Write-Output "Starting OOBE Script | DateTime: $($(Get-Date).ToString("yyyy-MM-dd HH:mm:ss"))" | Out-File -FilePath C:\Temp\OOBE_Cust.log -Force -Append
